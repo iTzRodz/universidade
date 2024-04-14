cbb2e33e146ddf6bb0323ed37d17a75e75a66f5c
Rodolfo wsl <rndcsjj@gmail.com>
2024-04-14 11:30:06 -0300

    feat: add new test new pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
+import Contact from './pages/Contact.jsx';
+import ErrorPage from './pages/error/error-page.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: App,
+    errorElement: <ErrorPage/>
+  },
+  {
+    path: "/teste",
+    Component: Contact
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <>
+     <span>sadsadsa</span>
+    </>
+  );
+}
+
+export default Contact;
